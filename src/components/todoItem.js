81ee091def23095414019d77ff1907a2ca9a79df
Bryan <artificialknowledge217@gmail.com>
2017-05-11 02:13:35 -0400

    add checked prop

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function TodoItem (props) {
 	return (
 		<div className="item">
-			<h1>{props.itemName}</h1>
-			<button className="check action">Check</button>
-			<button className="delete action">Delete</button>
+			<h1 className={props.checked}>{props.itemName}</h1>
+			<button className="check action" id={props.id} onClick={props.onClick}>Check</button>
+			<button className="delete action" id={props.id} onClick={props.onClick}>Delete</button>
 		</div>
 	);
 }
