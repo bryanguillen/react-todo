b4434fb049377b6a7156267cec018d6e6283aa84
Bryan <artificialknowledge217@gmail.com>
2017-05-11 02:11:20 -0400

    add handleItem event

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import update from 'immutability-helper';
 import TodoInput from './todoInput';
 import TodoList from './todoList';
 
@@ -7,17 +8,21 @@ export default class TodoApp extends React.Component {
 		super(props);
 		this.handleInputChange = this.handleInputChange.bind(this);
 		this.handleItemSubmit = this.handleItemSubmit.bind(this);
+		this.handleItemEvent = this.handleItemEvent.bind(this);
 		this.state = {
 
 		items:	[
 					{ 
-						itemName: "Pick Up The Kids" 
+						itemName: "Pick Up The Kids",
+						checked: false 
 					},
 					{ 
-						itemName: "Get Groceries"
+						itemName: "Get Groceries",
+						checked: false
 					},
 					{ 
-						itemName: "Attend School"
+						itemName: "Attend School",
+						checked: false
 					}
 				],
 		item: ''
@@ -36,11 +41,34 @@ export default class TodoApp extends React.Component {
 		})
 	}
 
+	handleItemEvent (e) {
+		e.preventDefault();
+		
+		let classes = e.target.className.split(' ');
+		let itemId = e.target.id;
+		let checkedValue = this.state.items[itemId].checked;
+
+		if (classes.includes("delete")) {
+			let newData = update(this.state.items, {$splice: [[itemId, 1]]});
+			return this.setState({ items: newData });
+		}
+		
+		if (!checkedValue) {
+			let updatedObj = update(this.state.items[itemId], { checked: {$set: true } });
+			let newData = update(this.state.items, {$splice: [[itemId, 1, updatedObj]]});
+			return this.setState({ items: newData });
+		}
+
+		let updatedObj = update(this.state.items[itemId], { checked: {$set: false } });
+		let newData = update(this.state.items, {$splice: [[itemId, 1, updatedObj]]});
+		return this.setState({ items: newData });
+	}
+
 	render() {
 		return (
 			<div>
 				<TodoInput value={this.state.item} onChange={this.handleInputChange} onSubmit={this.handleItemSubmit} />
-				<TodoList items={this.state.items} />
+				<TodoList items={this.state.items} onClick={this.handleItemEvent} />
 			</div>
 		);
 	}
