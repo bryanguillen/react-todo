76821fa6c59c9f805704af7c900e5f5d058345f3
Bryan <artificialknowledge217@gmail.com>
2017-05-11 14:48:57 -0400

    Add provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import TodoApp from './components/todoApp';
 import './index.css';
 
+console.log(store.getState());
+
 ReactDOM.render(
-  <TodoApp />,
+  	<Provider store={store}>
+  		<TodoApp />
+  	</Provider>,
   document.getElementById('root')
 );
