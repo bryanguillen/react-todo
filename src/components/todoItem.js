42f7390b01acf9f62091502c6d4676fdeb8b3cc7
Bryan <artificialknowledge217@gmail.com>
2017-05-11 19:08:49 -0400

    add delte dispatcher

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { deleteItem } from '../actions/index';
 
-export default function TodoItem (props) {
-	return (
-		<div className="item">
-			<h1 className={props.checked}>{props.itemName}</h1>
-			<button className="check action" id={props.id} onClick={props.onClick}>Check</button>
-			<button className="delete action" id={props.id} onClick={props.onClick}>Delete</button>
-		</div>
-	);
+export class TodoItem extends React.Component {
+
+	deleteItem (index) {
+		this.props.dispatch(deleteItem(index));
+	}
+
+	render() {
+		return (
+			<div className="item">
+				<h1 className={this.props.checked}>{this.props.itemName}</h1>
+				<button className="check action" id={this.props.id} onClick={this.checkButton}>Check</button>
+				<button className="delete action" id={this.props.id} 
+				onClick={ e => {
+					e.preventDefault();
+					let itemId = e.target.id;
+					this.deleteItem(itemId);
+				}}>Delete</button>
+			</div>
+		);
+	}
 }
+
+const mapStateToProps = state => ({
+	items: state.items
+})
+
+export default connect(mapStateToProps)(TodoItem) 
