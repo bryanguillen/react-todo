0df285853861da9a1230066d08593a47972326f3
Bryan <artificialknowledge217@gmail.com>
2017-05-11 02:14:02 -0400

    add business logic for determining rendering

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -3,7 +3,27 @@ import TodoItem from './todoItem';
 
 export default function TodoList(props) {
 		const renderedList = props.items.map(function (item, index) {
-			return <TodoItem key={index} itemName={item.itemName} />
+			
+			if (!item.checked) {
+				return (<TodoItem 
+						key={index}
+						id={index} 
+						itemName={item.itemName} 
+						onClick={props.onClick} 
+						/>
+				);
+			}
+
+			return (
+				<TodoItem 
+				key={index}
+				id={index} 
+				itemName={item.itemName} 
+				onClick={props.onClick}
+				checked="checked"
+				/>
+			);
+			
 		}) 
 		return (
 			<div className="todo-list">
