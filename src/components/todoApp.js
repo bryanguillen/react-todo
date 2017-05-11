5c70058c083725ad4631c3ca9c5037a2f5cbd628
Bryan <artificialknowledge217@gmail.com>
2017-05-11 00:22:26 -0400

    add todoList instead todoItem

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import TodoInput from './todoInput';
-import TodoItem from './todoItem';
+import TodoList from './todoList';
 
 export default class TodoApp extends React.Component {
 	constructor(props){
 		super(props);
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleItemSubmit = this.handleItemSubmit.bind(this);
+		this.state = {
+
+		items:	[
+					{ 
+						itemName: "Pick Up The Kids" 
+					},
+					{ 
+						itemName: "Get Groceries"
+					},
+					{ 
+						itemName: "Attend School"
+					}
+				],
+		item: ''
+		}
+	}
+
+	handleInputChange (e) {
+		this.setState({ item: e.target.value });
+	}
+
+	handleItemSubmit (e) {
+		e.preventDefault();
+		this.setState({ 
+			items: this.state.items.concat([{ itemName: this.state.item }]),
+			item: '' 
+		})
 	}
 
 	render() {
 		return (
 			<div>
-				<TodoInput/>
-				<TodoItem itemName="Pick Up the kids"/>
+				<TodoInput value={this.state.item} onChange={this.handleInputChange} onSubmit={this.handleItemSubmit} />
+				<TodoList items={this.state.items} />
 			</div>
 		);
 	}
