f3c168253730d58fd89ac7a70a3248143e416cf1
Bryan <artificialknowledge217@gmail.com>
2017-05-11 14:48:46 -0400

    handle all of the logic in this component

--- src/components/todoInput.js
+++ src/components/todoInput.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addItem } from '../actions/index.js';
 
-export default function TodoInput(props) {
+let TodoInput = ({dispatch}) => {
+	let input;
 	return (
 		<div className="input-container">
-			<form onSubmit={props.onSubmit} className="item-form">
-				<input onChange={props.onChange} value={props.value} id="item" type="text" placeholder="Pick Up The Kids" />
+			<form onSubmit={e => {
+				e.preventDefault();
+				if (!input.value.trim()) {
+					return
+				}
+				dispatch(addItem(input.value));
+				input.value = '';
+			}} 
+			className="item-form">
+				<input 
+				ref={node => { 
+					input = node
+				}} 
+				id="item" 
+				type="text" 
+				placeholder="Pick Up The Kids" />
 				<button type="submit" value="Submit" className="submit-item">Submit</button>
 			</form>
 		</div>
 	);
 }
+
+TodoInput = connect()(TodoInput);
+
+export default TodoInput
