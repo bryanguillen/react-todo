c56898000d00f0d9f78142fd10a7616a9af7c6c4
Bryan <artificialknowledge217@gmail.com>
2017-05-11 02:12:57 -0400

    add classname for form

--- src/components/todoInput.js
+++ src/components/todoInput.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function TodoInput(props) {
 	return (
 		<div className="input-container">
-			<form onSubmit={props.onSubmit}>
+			<form onSubmit={props.onSubmit} className="item-form">
 				<input onChange={props.onChange} value={props.value} id="item" type="text" placeholder="Pick Up The Kids" />
-				<button type="submit" value="Submit">Submit</button>
+				<button type="submit" value="Submit" className="submit-item">Submit</button>
 			</form>
 		</div>
 	);
